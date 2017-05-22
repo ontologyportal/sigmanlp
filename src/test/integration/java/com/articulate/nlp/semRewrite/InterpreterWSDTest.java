81c416fca50305be8973bdeb88bdc1d976464beb
apease <apease@articulatesoftware.com>
2017-05-21 18:30:16 -0700

    fix import

--- src/test/integration/java/com/articulate/nlp/semRewrite/InterpreterWSDTest.java
+++ src/test/integration/java/com/articulate/nlp/semRewrite/InterpreterWSDTest.java
@@ -36,7 +36,6 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.articulate.sigma.nlp.pipeline.SentenceUtil.toDependenciesList;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
