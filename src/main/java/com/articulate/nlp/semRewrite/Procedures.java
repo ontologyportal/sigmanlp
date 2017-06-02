f91edb8d8b4731f2871aa28daad1214ecb45542c
apease <apease@articulatesoftware.com>
2017-06-02 08:31:48 +0530

    Added debugging statements

--- src/main/java/com/articulate/nlp/semRewrite/Procedures.java
+++ src/main/java/com/articulate/nlp/semRewrite/Procedures.java
@@ -24,9 +24,14 @@ MA  02111-1307 USA
 
 import com.articulate.sigma.KB;
 import com.articulate.sigma.KBmanager;
+import com.articulate.sigma.Formula;
+
+import java.util.ArrayList;
 
 public class Procedures {
-    
+
+    public static boolean debug = false;
+
     /** ***************************************************************
      * CELT classes which are not SUMO classes, like "person"
      */
@@ -37,13 +42,14 @@ public class Procedures {
             return "true";
 
         KB kb = KBmanager.getMgr().getKB("SUMO");
-        //System.out.println("INFO in Procedures.isCELTclass(): " + c);
+        if (debug) System.out.println("INFO in Procedures.isCELTclass(): " + c);
         if (kb == null) {
             //if (c.arg1.equals("River") && c.arg2.equals("Object"))
             //    return "true";
             return "false";
         }
-        //System.out.println("INFO in Procedures.isCELTclass(): " + kb.isSubclass(c.arg1, c.arg2));
+        if (debug) System.out.println("INFO in Procedures.isCELTclass(): isSubclass" +
+                kb.isSubclass(c.arg1, c.arg2));
 
         if (c.arg2.equals("Person"))
             if (kb.isSubclass(c.arg1, "Human") || kb.isSubclass(c.arg1, "SocialRole"))
@@ -66,8 +72,9 @@ public class Procedures {
     public static String isSubclass(Literal c) {
 
         KB kb = KBmanager.getMgr().getKB("SUMO");
-        //System.out.println("INFO in Procedures.isSubclass(): " + c);
-        //System.out.println("INFO in Procedures.isSubclass(): " + kb.isSubclass(c.arg1, c.arg2));
+        if (debug) System.out.println("INFO in Procedures.isSubclass(): " + c);
+        if (debug) System.out.println("INFO in Procedures.isSubclass(): " +
+                kb.isSubclass(c.arg1, c.arg2));
         if (kb.isSubclass(c.arg1, c.arg2))
             return "true";
         else
@@ -79,13 +86,14 @@ public class Procedures {
     public static String isInstanceOf(Literal c) {
 
         KB kb = KBmanager.getMgr().getKB("SUMO");
-        //List<Formula> forms = kb.ask("arg",1,c.arg1);
-        //System.out.println("INFO in Procedures.isInstanceOf(): " + forms);
-        //Formula f = forms.get(0);
-        //System.out.println("INFO in Procedures.isInstanceOf(): " + f.getArgument(2));
-        //System.out.println("INFO in Procedures.isInstanceOf(): " + kb.isSubclass(f.getArgument(2),c.arg2));
-        //System.out.println("INFO in Procedures.isInstanceOf(): " + c);
-        //System.out.println("INFO in Procedures.isInstanceOf(): " + kb.isInstanceOf(c.arg1, c.arg2));
+        ArrayList<Formula> forms = kb.ask("arg",1,c.arg1);
+        if (debug) System.out.println("INFO in Procedures.isInstanceOf(): " + forms);
+        Formula f = null;
+        //if (forms != null && forms.size() > 0) forms.get(0);
+        //if (debug = true) System.out.println("INFO in Procedures.isInstanceOf(): " + f.getArgument(2));
+        //if (debug = true) System.out.println("INFO in Procedures.isInstanceOf(): " + kb.isSubclass(f.getArgument(2),c.arg2));
+        if (debug) System.out.println("INFO in Procedures.isInstanceOf(): " + c);
+        if (debug) System.out.println("INFO in Procedures.isInstanceOf(): " + kb.isInstanceOf(c.arg1, c.arg2));
         if (kb.isInstanceOf(c.arg1, c.arg2))
             return "true";
         else
