073780d6ba60ac3355eda0d95d92638fed02cc53
apease <apease@articulatesoftware.com>
2017-07-23 14:41:19 -0700

    updated comment header

--- src/main/java/com/articulate/nlp/WSDAnnotator.java
+++ src/main/java/com/articulate/nlp/WSDAnnotator.java
@@ -12,12 +12,29 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.util.ArraySet;
 import edu.stanford.nlp.util.CoreMap;
 
-/**
- * This class adds WordNet word senses and SUMO terms to tokens. It uses the
- * WSD class, SemCor statistics and WordNet-SUMO mappings. Assumes
- * that the Annotation has already been split into sentences, then tokenized into Lists of CoreLabels.
- *
- * @author apease
+/*
+Copyright 2017 Articulate Software
+
+Author: Adam Pease apease@articulatesoftware.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program ; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+MA  02111-1307 USA
+
+ This class adds WordNet word senses and SUMO terms to tokens. It uses the
+ WSD class, SemCor statistics and WordNet-SUMO mappings. Assumes
+ that the Annotation has already been split into sentences, then tokenized into Lists of CoreLabels.
  */
 
 public class WSDAnnotator implements Annotator {
