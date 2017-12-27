a75d8e2cb58a9ece7fb7557e414c21fd0823cf41
apease <apease@articulatesoftware.com>
2017-12-27 10:38:41 -0800

    add ant command

--- updateNLP.sh
+++ updateNLP.sh
@@ -1,6 +1,7 @@
 ../sigmakee/updateSigma.sh
 cd $ONTOLOGYPORTAL_GIT/sigmanlp
 git pull
+ant
 echo "waiting 10 seconds"
 sleep 10
 curl -d userName="user" -d password="user" "http://localhost:8080/sigmanlp/NLP.jsp" > result.html
