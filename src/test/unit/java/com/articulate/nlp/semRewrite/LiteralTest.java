79079692fb61160424cbf2b6d42ed5d73938b1f9
apease <apease@articulatesoftware.com>
2018-02-17 15:39:13 -0800

    new tests

--- src/test/unit/java/com/articulate/nlp/semRewrite/LiteralTest.java
+++ src/test/unit/java/com/articulate/nlp/semRewrite/LiteralTest.java
@@ -2,11 +2,31 @@ package com.articulate.nlp.semRewrite;
 
 import com.articulate.sigma.UnitTestBase;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-/**
- * Created by apease on 6/5/15.
- */
-public class LiteralTest extends UnitTestBase {
+import java.util.HashMap;
+
+/*
+Copyright 2017-     Infosys
+Author: Adam Pease apease@articulatesoftware.com
+
+This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 2 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program ; if not, write to the Free Software
+        Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+        MA  02111-1307 USA
+        */
+
+public class LiteralTest {
 
     /****************************************************************
      */
@@ -30,5 +50,51 @@ public class LiteralTest extends UnitTestBase {
         example = "root(ROOT-0, than-15), mark(3,200, Although-1)";
         lex = new Lexer(example);
         Literal.parse(lex,0);
+    }
+
+    /****************************************************************
+     */
+    public void testUnify(String lstr1, String lstr2, String resStr) {
+
+        Literal l = new Literal(lstr1);
+        Literal l2 = new Literal(lstr2);
+        HashMap<String,String> bindings = l.mguTermList(l2);
+        System.out.println("LiteralTest.testUnify(): " + lstr1);
+        System.out.println("LiteralTest.testUnify(): " + lstr2);
+        System.out.println("LiteralTest.testUnify(): result: " + bindings);
+        String result = null;
+        if (bindings != null)
+            result = bindings.toString();
+        assertEquals(resStr,result);
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testUnify1() {
+
+        String example1 = "root(ROOT-0, reached-14)";
+        String example2 = "root(?V, reached*)";
+        testUnify(example1,example2,"{?V=ROOT-0}");
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testUnify2() {
+
+        String example1 = "root(ROOT-0, reached-14)";
+        String example2 = "root(?V, lost*)";
+        testUnify(example1,example2,null);
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testUnify3() {
+
+        String example1 = "foo(ROOT-0, reached-14)";
+        String example2 = "root(?V, reached*)";
+        testUnify(example1,example2,null);
     }
 }
