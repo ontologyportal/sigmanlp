1de3afa50677094ecd0f13173cced6c16b55c8fb
apease <apease@articulatesoftware.com>
2018-03-11 18:09:23 -0700

    add tests

--- src/test/integration/java/com/articulate/nlp/semRewrite/RelExtractTest.java
+++ src/test/integration/java/com/articulate/nlp/semRewrite/RelExtractTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 
@@ -48,7 +49,7 @@ public class RelExtractTest extends IntegrationTestBase {
         interpreter = new Interpreter();
         interpreter.inference = false;
         Interpreter.debug = true;
-        Interpreter.replaceInstances = true;
+        Interpreter.replaceInstances = false;
         //CNF.debug = true;
         //Procedures.debug = true;
         //KBcache.debug = true;
@@ -65,13 +66,25 @@ public class RelExtractTest extends IntegrationTestBase {
 
         Lexer lex = new Lexer(input);
         CNF cnfInput = CNF.parseSimple(lex);
+
         Rule r = new Rule();
         r = Rule.parseString(rule);
-        CNF cnf = Clausifier.clausify(r.lhs);
+        r.cnf = Clausifier.clausify(r.lhs);
         System.out.println();
         System.out.println("INFO in RelExtractTest.doRuleTest(): Input: " + cnfInput);
-        System.out.println("INFO in RelExtractTest.doRuleTest(): CNF rule antecedent: " + cnf);
-        HashMap<String, String> bindings = cnf.unify(cnfInput);
+        System.out.println("INFO in RelExtractTest.doRuleTest(): CNF rule antecedent: " + r.cnf);
+
+        HashSet<String> preds = cnfInput.getPreds();
+        HashSet<String> terms = cnfInput.getTerms();
+        System.out.println("RelExtractTest.doRuleTest(): input preds: " + preds);
+        System.out.println("RelExtractTest.doRuleTest(): input terms: " + terms);
+
+        System.out.println("RelExtractTest.doRuleTest(): rule preds: " + r.preds);
+        System.out.println("RelExtractTest.doRuleTest(): rule terms: " + r.terms);
+
+        if (!interpreter.termCoverage(preds,terms,r))
+            System.out.println("RelExtractTest.doRuleTest(): no coverage for rule ");
+        HashMap<String, String> bindings = r.cnf.unify(cnfInput);
         System.out.println("bindings: " + bindings);
         RHS res = r.rhs.applyBindings(bindings);
         System.out.println("result: " + res);
@@ -83,14 +96,13 @@ public class RelExtractTest extends IntegrationTestBase {
     }
 
     /****************************************************************
+     * @param input is a sentence
      */
     private void doOneResultTest(String input, String expectedOutput) {
 
-        ArrayList<CNF> inputs = Lists.newArrayList(interpreter.interpretGenCNF(input));
         System.out.println();
         System.out.println("-------------------");
         System.out.println("INFO in RelExtractTest.doTest(): Input: " + input);
-        System.out.println("INFO in RelExtractTest.doTest(): CNF input: " + inputs);
         ArrayList<String> kifClauses = RelExtract.sentenceExtract(input);
         System.out.println("INFO in RelExtractTest.doTest(): result: " + kifClauses);
         System.out.println("INFO in RelExtractTest.doTest(): expected: " + expectedOutput);
@@ -172,7 +184,6 @@ public class RelExtractTest extends IntegrationTestBase {
     @Test
     public void testSimpleSent1() {
 
-        Interpreter.debug = true;
         System.out.println("INFO in RelExtractTest.testSimpleSent1()");
         String input = "Robert wears a shirt";
         String expected = "(wears Robert-1 shirt-4)";
@@ -347,7 +358,7 @@ public class RelExtractTest extends IntegrationTestBase {
         System.out.println("INFO in RelExtractTest.testSentHakawatiTheatre()");
         String input = " An art exhibit at the Hakawati Theatre in Arab east Jerusalem " +
                 "was a series of portraits of Palestinians killed in the rebellion .";
-        String expected = "(located Hakawati_Theatre-6 JerusalemIsrael)";
+        String expected = "(located Hakawati_Theatre-6 Jerusalem-11)";
 
         doOneResultTest(input, expected);
     }
@@ -381,6 +392,109 @@ public class RelExtractTest extends IntegrationTestBase {
         System.out.println("INFO in RelExtractTest.testUnify11(): cnf " + cnf4);
         System.out.println("INFO in RelExtractTest.testUnify11(): cnf1 " + cnf5);
         System.out.println("result: " + cnf4.unify(cnf5).toString());
-        assertEquals("{}",cnf4.unify(cnf5).toString());
+        assertEquals("{?X=Human, ?Y=baz}",cnf4.unify(cnf5).toString());
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testDepMadonna() {
+
+        System.out.println("INFO in RelExtractTest.testDepMadonna()");
+        String input = "amod(Detroiter-5,True), amod(Detroiter-5,native-4), amod(peninsula-32,lower-31), amod(place-23,nice-22), " +
+                "appos(Bay_City-16,Mich.-19), appos(Bay_City-16,place-23), case(Bay_City-16,from-15), case(Detroiter-5,As-1), " +
+                "case(peninsula-32,of-27), case(state-29,'s-30), case(thumb-26,in-24), ccomp(everyone-11,Bay_City-16), " +
+                "cop(Bay_City-16,is-14), det(Detroiter-5,a-2), det(place-23,a-21), det(state-29,the-28), det(thumb-26,the-25), " +
+                "dobj(remind-10,everyone-11), lemma(Bay,Bay-16), lemma(City,City-17), lemma(Madonna,Madonna-13), " +
+                "lemma(Mich.,Mich.-19), lemma(be,is-14), lemma(detroiter,Detroiter-5), lemma(everyone,everyone-11), " +
+                "lemma(peninsula,peninsula-32), lemma(place,place-23), lemma(remind,remind-10), lemma(state,state-29), " +
+                "lemma(thumb,thumb-26), lemma(want,want-8), mark(Bay_City-16,that-12), mark(remind-10,to-9), " +
+                "names(Madonna-13,\"Madonna\"), nmod:as(want-8,Detroiter-5), nmod:in(place-23,thumb-26), " +
+                "nmod:of(thumb-26,peninsula-32), nmod:poss(peninsula-32,state-29), nsubj(Bay_City-16,Madonna-13), " +
+                "nsubj(want-8,I-7), number(SINGULAR,Bay-16), number(SINGULAR,City-17), number(SINGULAR,Detroiter-5), " +
+                "number(SINGULAR,Madonna-13), number(SINGULAR,Mich.-19), number(SINGULAR,everyone-11), " +
+                "number(SINGULAR,peninsula-32), number(SINGULAR,place-23), number(SINGULAR,state-29), number(SINGULAR,thumb-26), " +
+                "root(ROOT-0,want-8), sumo(Attribute,is-14), sumo(Attribute,state-29), sumo(Bay,Bay_City-16), " +
+                "sumo(City,Bay_City-16), sumo(Human,Madonna-13), sumo(Peninsula,peninsula-32), sumo(PsychologicalAttribute,want-8), " +
+                "sumo(Region,place-23), sumo(Reminding,remind-10), sumo(StateOrProvince,Mich.-19), " +
+                "sumo(SubjectiveAssessmentAttribute,native-4), sumo(SubjectiveStrongPositiveAttribute,nice-22), " +
+                "sumo(Thumb,thumb-26), sumo(TruthValue,True), tense(PRESENT,is-14), tense(PRESENT,want-8), xcomp(want-8,remind-10)";
+
+        String rule = "nsubj(?LOC, ?animal-8), cop(?LOC, ?COP), case(?LOC, from*), isSubclass(?TYPEVAR0,Animal), " +
+                "sumo(?TYPEVAR0,?animal-8), isSubclass(?TYPEVAR1,Object), sumo(?TYPEVAR1,?LOC) ==> {(birthplace ?animal-8 ?LOC)}..";
+        String expected = "(birthplace Madonna-13 Bay_City-16)";
+
+        doRuleTest(input, rule, expected);
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testDepWilson() {
+
+        System.out.println("INFO in RelExtractTest.testDepWilson()");
+        String input = "advcl(be_born-5,counted-25), advmod(be_born-5,True), advmod(counted-25,hardly-24), advmod(counted-25,manner), " +
+                "amod(fame-15,political-14), amod(terms-31,political-30), case(Governor,as-16), case(NewJersey,of-18), " +
+                "case(Southerner-28,as-26), case(Virginia,in-7), case(terms-31,in-29), cc(be_born-5,but-10), conj:but(be_born-5,Won), " +
+                "det(Southerner-28,a-27), dobj(Won,fame-15), lemma(Jersey,Jersey-20), lemma(New,New-19), lemma(Virginia,Virginia-8), " +
+                "lemma(Wilson,Wilson-4), lemma(Woodrow,Woodrow-3), lemma(be,was-5), lemma(bear,born-6), lemma(count,counted-25), " +
+                "lemma(fame,fame-15), lemma(governor,governor-17), lemma(southerner,Southerner-28), lemma(term,terms-31), " +
+                "lemma(win,won-12), nmod:as(Won,Governor), nmod:as(counted-25,Southerner-28), nmod:in(Southerner-28,terms-31), " +
+                "nmod:in(be_born-5,Virginia), nmod:of(Governor,NewJersey), nmod:poss(fame-15,his-13), nsubj(Won,he-11), " +
+                "nsubj(counted-25,he-23), nsubjpass(be_born-5,PresidentOfTheUnitedStates), number(PLURAL,terms-31), " +
+                "number(SINGULAR,Jersey-20), number(SINGULAR,New-19), number(SINGULAR,Southerner-28), number(SINGULAR,Virginia-8), " +
+                "number(SINGULAR,Wilson-4), number(SINGULAR,Woodrow-3), number(SINGULAR,fame-15), number(SINGULAR,governor-17), " +
+                "root(ROOT-0,be_born-5), sumo(AmericanState,NewJersey), sumo(AmericanState,Virginia), sumo(AsymmetricRelation,manner), " +
+                "sumo(Birth,be_born-5), sumo(ContestAttribute,Won), sumo(Counting,counted-25), sumo(IrreflexiveRelation,manner), " +
+                "sumo(NounPhrase,terms-31), sumo(PoliticalOrganization,political-14), sumo(PoliticalOrganization,political-30), " +
+                "sumo(Position,Governor), sumo(Position,PresidentOfTheUnitedStates), sumo(President,PresidentOfTheUnitedStates), " +
+                "sumo(SubjectiveAssessmentAttribute,Southerner-28), sumo(SubjectiveAssessmentAttribute,fame-15), " +
+                "sumo(SubjectiveAssessmentAttribute,hardly-24), sumo(TotalValuedRelation,manner), sumo(TruthValue,True), " +
+                "sumo(property,manner), tense(PAST,counted-25), tense(PAST,was-5), tense(PAST,won-12)";
+
+        // nmod:in(be_born-5,Virginia), nsubjpass(be_born-5,PresidentOfTheUnitedStates), sumo(AmericanState,Virginia)
+        String rule = "nmod:in(be_born*,?LOC), nsubjpass(be_born*,?animal-8), isCELTclass(?animal-8,Person), isSubclass(?TYPEVAR1,Object), " +
+                "sumo(?TYPEVAR1,?LOC) ==> {(birthplace ?animal-8 ?LOC)}.";
+        String expected = "(birthplace PresidentOfTheUnitedStates Virginia)";
+
+        doRuleTest(input, rule, expected);
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testDepWilson2() {
+
+        //Interpreter.debug = true;
+        //CNF.debug = true;
+        //Procedures.debug = true;
+        System.out.println("INFO in RelExtractTest.testDepWilson2()");
+        String input = "nmod:in(be_born-5,Virginia), nsubjpass(be_born-5,PresidentOfTheUnitedStates), " +
+                "sumo(President,PresidentOfTheUnitedStates), sumo(AmericanState,Virginia).";
+
+        // nmod:in(be_born-5,Virginia), nsubjpass(be_born-5,PresidentOfTheUnitedStates), sumo(AmericanState,Virginia)
+        String rule = "nmod:in(be_born*,?LOC), nsubjpass(be_born*,?animal-8), isCELTclass(?animal-8,Person), isSubclass(?TYPEVAR1,Object), " +
+                "sumo(?TYPEVAR1,?LOC) ==> {(birthplace ?animal-8 ?LOC)}.";
+        String expected = "(birthplace PresidentOfTheUnitedStates Virginia)";
+
+        doRuleTest(input, rule, expected);
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testSentWilson() {
+
+        //RelExtract.debug = true;
+        //CNF.debug = true;
+        //Procedures.debug = true;
+        System.out.println("INFO in RelExtractTest.testDepWilson2()");
+        String input = "True , Woodrow Wilson was born in Virginia , but he won his political " +
+                "fame as governor of New Jersey , so he hardly counted as a Southerner in political terms .";
+
+        // nmod:in(be_born-5,Virginia), nsubjpass(be_born-5,PresidentOfTheUnitedStates), sumo(AmericanState,Virginia)
+        String expected = "(birthplace Woodrow_Wilson-3 Virginia-8)";
+
+        doOneResultTest(input, expected);
     }
 }
