473b148f40d57daaeb4926ebbd016ff0c72474d2
apease <apease@articulatesoftware.com>
2018-03-17 18:40:15 -0700

    fix redirect

---
+++ src/test/unit/java/com/articulate/nlp/semRewrite/RHSTest.java
@@ -0,0 +1,50 @@
+package com.articulate.nlp.semRewrite;
+
+import com.articulate.nlp.IntegrationTestBase;
+import com.articulate.sigma.Formula;
+import com.articulate.sigma.FormulaUtil;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/*
+Copyright 2017-     Infosys
+Author: Adam Pease apease@articulatesoftware.com
+
+This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation; either version 2 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program ; if not, write to the Free Software
+        Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+        MA  02111-1307 USA
+        */
+public class RHSTest extends IntegrationTestBase {
+
+    /****************************************************************
+     */
+    @Test
+    public void testRHSCNF() {
+
+        RHS.debug = true;
+        String input = "nsubj(grasps*,?animal), dobj(grasps*,?object), sumo(?TYPEVAR1,?object), " +
+                "isCELTclass(?animal,Person), isChildOf(?TYPEVAR1,Object) ==> {(grasps ?animal ?object)}.";
+        Lexer lex = new Lexer(input);
+        Rule r = Rule.parse(lex);
+        System.out.println("RHSTest.testRHSCNF(): " + r.rhs.cnf);
+        assert(r.rhs.cnf != null);
+    }
+
+
+}
