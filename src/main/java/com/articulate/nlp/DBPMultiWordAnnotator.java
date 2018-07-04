a3c4648e5bf838a438e06342a977aa3ad987b334
apease <apease@articulatesoftware.com>
2018-07-04 08:47:35 -0700

    refactored most of the code into a parent class MultiWordAnnotator

--- src/main/java/com/articulate/nlp/DBPMultiWordAnnotator.java
+++ src/main/java/com/articulate/nlp/DBPMultiWordAnnotator.java
@@ -1,13 +1,15 @@
 package com.articulate.nlp;
 
+import com.articulate.nlp.corpora.DBPedia;
 import com.articulate.sigma.*;
-import com.articulate.sigma.dbpedia.DBPedia;
+import com.articulate.sigma.wordNet.MultiWords;
 import com.articulate.sigma.wordNet.WSD;
 import com.articulate.sigma.wordNet.WordNet;
 import com.articulate.sigma.wordNet.WordNetUtilities;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.Annotator;
 
+import java.io.File;
 import java.util.*;
 
 import edu.stanford.nlp.ling.CoreAnnotation;
@@ -23,7 +25,7 @@ import edu.stanford.nlp.util.IntPair;
  * @author apease
  */
 
-public class DBPMultiWordAnnotator implements Annotator {
+public class DBPMultiWordAnnotator extends MultiWordAnnotator {
 	
     // Each CoreLabel in a multi-word string gets one that provides the entire
     // multi-word in DBP format.  Individual words will still have their own
@@ -41,7 +43,7 @@ public class DBPMultiWordAnnotator implements Annotator {
     }
 
     //spans are set to token numbers not array position index
-    public static class DBPMWSpanAnnotation  extends CoreAnnotations.SpanAnnotation {
+    public static class DBPMWSpanAnnotation extends CoreAnnotations.SpanAnnotation {
         public Class<IntPair> getType() {
             return IntPair.class;
         }
@@ -53,135 +55,16 @@ public class DBPMultiWordAnnotator implements Annotator {
         }
     }
 
-    public static boolean debug = false;
-
-    /** ***************************************************************
-     * Find the synset for a multi-word string, if it exists.
-     *
-     * @param tokens is a List of CoreLabel words.
-     * @param startIndex is the first word in the list to look at,
-     *                   note that token numbers start at 1 but list inices
-     *                   start at 0
-     * @param synset is an array of only one element, if a synset is found
-     * and empty otherwise
-     * @return the index into the next word to be checked, in text,
-     * which could be the same as startIndex, if no multi-word was found
-     */
-    public static int findMultiWord(List<CoreLabel> tokens, int startIndex, List<String> synset,
-                             StringBuffer multiWordToken) {
-
-    	if(debug) System.out.println("INFO in DBPMultiWordAnnotator.findMultiWord(): tokens: '" + tokens + "'");
-        if(debug) System.out.println("INFO in DBPMultiWordAnnotator.findMultiWord(): start: '" + startIndex + "'");
-        if(debug) System.out.println("INFO in DBPMultiWordAnnotator.findMultiWord(): synset: '" + synset + "'");
-        if(debug) System.out.println("INFO in DBPMultiWordAnnotator.findMultiWord(): multiWordToken: '" + multiWordToken + "'");
-        
-        if (startIndex + 1 < tokens.size())
-            return startIndex + findMultiWord(tokens.get(startIndex),
-                tokens.subList(startIndex + 1, tokens.size()), synset, multiWordToken);
-        else
-            return startIndex;
-    }
-
-    /** ***************************************************************
-     *  We need to try both the root form and the original form,
-     *  which includes capitalized and lower case versions.
-     *  @return the count of words in the multi-word expression
-     *  put the variable token in foundMultiWord
-     */
-    public static int findMultiWord(CoreLabel token, List<CoreLabel> multiWordTail, List<String> synset,
-                                    StringBuffer foundMultiWord) {
-
-        StringBuffer currentMultiWord = new StringBuffer();
-        String multiWordKey = token.lemma();
-        if (!DBPedia.dbp.getMultiWords().dbPediaMultiWord.containsKey(multiWordKey))
-            multiWordKey = token.originalText();
-        int wordIndex = 0;
-        int endIndex = 0;
-        String sense = "";
-        if (DBPedia.dbp.getMultiWords().dbPediaMultiWord.containsKey(multiWordKey) && !multiWordTail.isEmpty()) {
-            int mwlen = foundMultiWord.length();
-            currentMultiWord.delete(0,mwlen);
-            currentMultiWord = currentMultiWord.append(multiWordKey + "_" + multiWordTail.get(wordIndex).originalText());
-            Collection<String> candidates = DBPedia.dbp.getMultiWords().dbPediaMultiWord.get(multiWordKey);
-            while (candidates.size() > 0) {
-                ArrayList<String> newCandidates = new ArrayList<String>();
-                for (String candidate : candidates) {
-                    if (candidate.equals(currentMultiWord.toString())) {
-                        sense = WSD.getBestDefaultSense(currentMultiWord.toString());
-                        foundMultiWord.replace(0,currentMultiWord.length(),currentMultiWord.toString());
-                        if (!StringUtil.emptyString(sense)) {
-                            endIndex = wordIndex + 2;
-                        }
-                    }
-                    else if (candidate.startsWith(currentMultiWord.toString())) {
-                        newCandidates.add(candidate);
-                    }
-                }
-                if (newCandidates.size() > 0) {
-                    if (wordIndex > multiWordTail.size() - 1) {
-                        candidates = new ArrayList<String>();  // ran out of words, trigger an exit
-                    }
-                    else {
-                        candidates = newCandidates;
-                        wordIndex++;
-                        if (wordIndex < multiWordTail.size())
-                            currentMultiWord.append("_" + multiWordTail.get(wordIndex).originalText());
-                    }
-                }
-                else {
-                    candidates = new ArrayList<>();
-                }
-            }
-        }
-        synset.add(sense);
-        return endIndex;
-    }
-
     /****************************************************************
      */
-    public static void annotateSentence(List<CoreLabel> tokens) {
+    public DBPMultiWordAnnotator() {
 
-        if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): start: " + tokens);
-        int wordIndex = 0;
-        for (CoreLabel token : tokens) {
-            if (token.index() < wordIndex) // skip the found multi-word
-                continue;
-            int i = token.index() - 1 ;
-            ArrayList<String> multiWordResult = new ArrayList<String>();
-            StringBuffer multiWordToken = new StringBuffer();
-
-            if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): start: " + tokens);
-            if (debug) System.out.println("Before <DBPedia findMultiWord> millis: " + System.currentTimeMillis());
-            wordIndex = findMultiWord(tokens, i, multiWordResult, multiWordToken);
-            if (debug) System.out.println("Atter <DBPedia findMultiWord> millis: " + System.currentTimeMillis());
-            
-            if (multiWordToken.length() > 0 && debug)
-                System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): found multi-word: " + multiWordToken);
-            if (multiWordToken.length() > 0)
-                multiWordToken.append("-" +  Integer.toString(token.index())); // set to token number not list index
-            if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): start: " + i + " end: " + wordIndex);
-            if (wordIndex != i && multiWordToken.length() > 0) {
-                String synset = multiWordResult.get(0);
-                for (int index = i; index < wordIndex; index++) {
-                    CoreLabel tok = tokens.get(index);  // note that token index is token number -1
-                    tok.set(DBPMultiWordAnnotation.class,synset);
-                    IntPair ip = new IntPair(i+1,wordIndex); // spans are set to token numbers
-                    if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): set span to: " +
-                            i + ":" + (wordIndex-1));
-                    tok.set(DBPMWSpanAnnotation.class,ip);
-                    String sumo = WordNetUtilities.getBareSUMOTerm(WordNet.wn.getSUMOMapping(synset));
-                    if (!StringUtil.emptyString(sumo))
-                        tok.set(DBPMWSUMOAnnotation.class,sumo);
-                    if (!StringUtil.emptyString(multiWordToken.toString()))
-                        tok.set(DBPMWTokenAnnotation.class,multiWordToken.toString());
-                    if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): set MW synset for token: " + tok);
-                    if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): set MW synset for index: " + index);
-                    if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): set sumo: " + sumo);
-                    if (debug) System.out.println("INFO in DBPediaMultiWordAnnotator.annotate(): set token: " +
-                            multiWordToken.toString() + " sumo: " + sumo + " ip: " + ip);
-                }
-            }
-        }
+        multiWordAnno = DBPMultiWordAnnotation.class;
+        sumoAnno = DBPMWSUMOAnnotation.class;
+        spanAnno = DBPMWSpanAnnotation.class;
+        tokenAnno = DBPMWTokenAnnotation.class;
+        String path = System.getenv("CORPORA") + File.separator + "DBPedia" + File.separator;
+        DBPedia.readPageToString(path);
     }
     
     /****************************************************************
