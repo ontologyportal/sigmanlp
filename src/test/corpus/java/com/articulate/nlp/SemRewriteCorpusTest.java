05e8a93c2dca2ad99c79bf93033cb074edb2d9d6
apease <apease@articulatesoftware.com>
2019-05-17 11:33:03 -0700

    add new tests

---
+++ src/test/corpus/java/com/articulate/nlp/SemRewriteCorpusTest.java
@@ -0,0 +1,79 @@
+package com.articulate.nlp.semRewrite;
+
+import com.articulate.sigma.Formula;
+import com.articulate.nlp.IntegrationTestBase;
+import com.google.common.collect.Sets;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests the SemRewrite of a given parse.
+ * In: Dependency parse as string.
+ * Out: The right-hand-side of a SemRewrite rule that is triggered by the input.
+ */
+public class SemRewriteCorpusTest extends IntegrationTestBase {
+
+    private static Interpreter interpreter;
+
+    @Before
+    public void setUpInterpreter() throws IOException {
+
+        interpreter = new Interpreter();
+        interpreter.inference = false;
+        interpreter.initialize();
+    }
+
+    /****************************************************************
+     */
+    private void doTest(String input, String expected) {
+
+        if (input == null || input == "" || expected == null || expected == "") {
+            System.out.println("Error in SemRewriteCorpusTest.doTest(): null input");
+            return;
+        }
+        Formula fExpected = new Formula(expected);
+        String actual = interpreter.interpretSingle(input);
+        Formula fActual = new Formula(actual);
+        System.out.println("SemRewriteCorpusTest: input: " + input);
+        System.out.println("SemRewriteCorpusTest: expected: " + fExpected);
+        System.out.println("SemRewriteCorpusTest: actual: " + fActual);
+        //Formula.debug = true;
+        if (fExpected.deepEquals(fActual))
+            System.out.println("SemRewriteCorpusTest: pass");
+        else
+            System.out.println("SemRewriteCorpusTest: fail");
+        assertTrue(fExpected.deepEquals(fActual));
+    }
+
+    /****************************************************************
+     */
+    @Test
+    public void testLeighBlankets()   {
+
+        String input = "Leigh swaddled the baby in blankets.";
+        String expected = "(exists (?Leigh-1 ?baby-4 ?blankets-6 ?swaddled-2)\n" +
+                "  (and\n" +
+                "    (orientation ?swaddled-2 ?blankets-6 Inside)\n" +
+                "    (destination ?swaddled-2 ?blankets-6)\n" +
+                "    (names ?Leigh-1 \"Leigh\")\n" +
+                "    (instance ?baby-4 HumanBaby)\n" +
+                "    (agent ?swaddled-2 ?Leigh-1)\n" +
+                "    (patient ?swaddled-2 ?baby-4)\n" +
+                "    (earlier\n" +
+                "      (WhenFn ?swaddled-2) Now)\n" +
+                "    (instance ?blankets-6 Blanket)\n" +
+                "    (instance ?Leigh-1 Human)\n" +
+                "    (instance ?swaddled-2 Covering)))";
+        doTest(input,expected);
+    }
+
+}
