63f1c25f25c099e4176af7f0a53bb1c1b0eb31fb
apease <apease@articulatesoftware.com>
2019-06-13 07:22:39 -0700

    added more test classes that are passing to summary class to execute them all

--- src/test/integration/java/com/articulate/nlp/semRewrite/RunAllSemRewriteIntegTest.java
+++ src/test/integration/java/com/articulate/nlp/semRewrite/RunAllSemRewriteIntegTest.java
@@ -6,7 +6,10 @@ import com.articulate.nlp.semRewrite.datesandnumber.*;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        InterpreterPreprocessTest.class,
         InterpretNumericsTest.class,
+        ProceduresTest.class,
+        RelExtractTest.class,
         RulePrepAboutRefersToIntegrationTest.class,
         SentenceUtilTest.class,
         UnificationTest.class
