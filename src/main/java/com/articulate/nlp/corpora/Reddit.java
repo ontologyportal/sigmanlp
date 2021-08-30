eb4ff273d6df58650ce44b28706971f7f2bffe01
apease <apease@articulatesoftware.com>
2021-08-30 07:14:25 -0700

    extract more text content

--- src/main/java/com/articulate/nlp/corpora/Reddit.java
+++ src/main/java/com/articulate/nlp/corpora/Reddit.java
@@ -1,5 +1,6 @@
 package com.articulate.nlp.corpora;
 
+import com.articulate.sigma.utils.FileUtil;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -10,46 +11,48 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Reddit {
 
+    /** ***************************************************************
+     */
     public static void extract(String input) {
 
         StringBuilder contentBuilder = new StringBuilder();
-
-        try (Stream<String> stream = Files.lines( Paths.get(input), StandardCharsets.UTF_8)) {
-            stream.forEach(s -> contentBuilder.append(s).append("\n"));
-            Pattern p = Pattern.compile("1qeIAgB0cPwnLhDF9XSiJM\">([^<]*)</p>");
-            Matcher m = p.matcher(contentBuilder.toString());
-            while (m.find()) {
-                System.out.println(m.group(1));
-            }
+        Collection<String> files = FileUtil.walk(input);
+        //System.out.println("Reddit.extract(): # files: " + files.size());
+        for (String s : files)
+            contentBuilder.append(FileUtil.readLines(s,false).toString()).append("\n");
+        Pattern p = Pattern.compile("1qeIAgB0cPwnLhDF9XSiJM\">([^<]*)</p>");
+        Matcher m = p.matcher(contentBuilder.toString());
+        while (m.find()) {
+            String text = m.group(1);
+            text = text.replace("&#x27;","'");
+            text = text.replace("[removed]","");
+            text = text.replace("(nsfw)","'");
+            System.out.println(text);
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public static void findFnames(String sDir) {
-
-        //System.out.println("findFnames(): " + sDir);
-        File[] faFiles = new File(sDir).listFiles();
-        for (File file: faFiles){
-            if (file.getName().endsWith(".html")){
-                extract(file.getAbsolutePath());
-            }
-            if (file.isDirectory()){
-                findFnames(file.getAbsolutePath());
-            }
+        p = Pattern.compile("\"e\":\"text\",\"t\":\"([^\"]*)\"");
+        m = p.matcher(contentBuilder.toString());
+        while (m.find()) {
+            String text = m.group(1);
+            text = text.replace("&#x27;","'");
+            System.out.println(m.group(1));
         }
     }
+
+    /** ***************************************************************
+     */
     public static void main(String[] args) {
 
         String input = System.getenv("CORPORA") + File.separator + args[0];
-        findFnames(input);
+        extract(input);
     }
 
 }
