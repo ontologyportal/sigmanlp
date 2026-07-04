897df232f0abeef0510b49fab79c8b51b98c02af
ShaunRose <shaunrose831@gmail.com>
2026-07-03 22:49:38 -0700

    Testing default initialization with Github .sigmakee/KBs/config.xml

--- src/test/integration/java/com/articulate/nlp/IntegrationTestBase.java
+++ src/test/integration/java/com/articulate/nlp/IntegrationTestBase.java
@@ -51,91 +51,136 @@ public class IntegrationTestBase extends SigmaTestBase {
     public static void setup() throws IOException {
 
         long startTime = System.currentTimeMillis();
-        if (!integrationConfigurationInitialized || !KBmanager.initialized) {
-            Configuration config = buildIntegrationConfiguration();
-            System.out.println("IntegrationTestBase.setup(): initializing integration KB.");
-            KBmanager.initialized = false;
-            KBmanager.initializing = false;
-            KBmanager.getMgr().kbs.clear();
-            KBmanager.getMgr().initializeOnce(config);
-            integrationConfigurationInitialized = true;
+
+        System.out.println("IntegrationTestBase.setup(): using default KBmanager configuration.");
+        System.out.println("SIGMA_HOME = " + System.getenv("SIGMA_HOME"));
+        System.out.println("Expected runtime config = " +
+                new File(System.getenv("SIGMA_HOME"), "KBs/config.xml").getCanonicalPath());
+
+        if (!KBmanager.initialized) {
+            KBmanager.getMgr().initializeOnce();
         }
-        else {
-            System.out.println("IntegrationTestBase.setup(): reusing already initialized integration KB.");
-        }
-        kb = KBmanager.getMgr().getKB(KBmanager.getMgr().getDefaultKbName());
-        if (kb == null)
-            throw new IllegalStateException("Could not get default KB after initialization.");
-        if (kbBackup == null)
-            kbBackup = new KB(kb);
+
+        kb = KBmanager.getMgr().getKB("SUMO");
+        kbBackup = new KB(kb);
+
         checkConfiguration();
+
         long endTime = System.currentTimeMillis();
-        if (IntegrationTestBase.totalKbMgrInitTime == Long.MAX_VALUE)
+
+        if (IntegrationTestBase.totalKbMgrInitTime == Long.MAX_VALUE) {
             IntegrationTestBase.totalKbMgrInitTime = endTime - startTime;
+        }
     }
     
     private static Configuration buildIntegrationConfiguration() {
 
+        String sigmaHome = System.getenv("SIGMA_HOME");
         String kbDir = KB_PATH;
+        String userHome = System.getProperty("user.home");
+
         Configuration config = new Configuration();
         config.clearKbConstituentLists();
-        config.setPreference("baseDir", System.getenv("SIGMA_HOME"));
-        config.setPreference("kbDir", kbDir);
-        config.setPreference("inferenceTestDir", kbDir + File.separator + "tests");
-        config.setPreference("loadFresh", "true");
-        config.setPreference("loadLexicons", "true");
-        config.setPreference("cache", "true");
-        config.setPreference("cacheDisjoint", "true");
-        config.setPreference("cwa", "false");
-        config.setPreference("maxPredicateArity", "6");
-        config.setPreference("graphVizExec", "/usr/bin/dot");
-        String userHome = System.getProperty("user.home");
+
         String eproverExec = new File("/usr/local/bin/e_ltb_runner").canExecute()
                 ? "/usr/local/bin/e_ltb_runner"
-                : userHome + File.separator + "Programs" + File.separator + "E" + File.separator + "PROVER" + File.separator + "e_ltb_runner";
+                : userHome + File.separator + "Programs" + File.separator + "E" +
+                File.separator + "PROVER" + File.separator + "e_ltb_runner";
+
         String vampireExec = new File("/usr/local/bin/vampire").canExecute()
                 ? "/usr/local/bin/vampire"
-                : userHome + File.separator + "Programs" + File.separator + "vampire" + File.separator + "build" + File.separator + "vampire";
+                : userHome + File.separator + "Programs" + File.separator + "vampire" +
+                File.separator + "build" + File.separator + "vampire";
+
+        config.setPreference("adminBrowserLimit", "200");
+        config.setPreference("baseDir", sigmaHome);
+        config.setPreference("cache", "yes");
+        config.setPreference("celtdir", "");
+        config.setPreference("editDir", kbDir);
+        config.setPreference("eprover", eproverExec);
         config.setPreference("eproverExec", eproverExec);
+        config.setPreference("graphVizDir", "/usr/bin");
+        config.setPreference("graphVizExec", "/usr/bin/dot");
+        config.setPreference("holdsPrefix", "no");
+        config.setPreference("hostname", "localhost");
+        config.setPreference("https", "false");
+        config.setPreference("imageFormat", "png");
+        config.setPreference("inferenceTestDir", kbDir + File.separator + "tests");
+        config.setPreference("jedit", "/usr/share/jedit/jedit");
+        config.setPreference("kbDir", kbDir);
+        config.setPreference("leoExecutable", userHome + File.separator + "leo");
+        config.setPreference("lineNumberCommand", "");
+        config.setPreference("loadCELT", "no");
+        config.setPreference("loadFresh", "false");
+        config.setPreference("logDir", sigmaHome + File.separator + "logs");
+        config.setPreference("logLevel", "warning");
+        config.setPreference("port", "8080");
+        config.setPreference("prolog", "");
+        config.setPreference("reportDup", "no");
+        config.setPreference("reportFnError", "no");
+        config.setPreference("showcached", "no");
+        config.setPreference("sumokbname", "SUMO");
+        config.setPreference("systemsDir", userHome);
+        config.setPreference("termFormats", "no");
+        config.setPreference("testOutputDir", System.getenv("CATALINA_HOME") +
+                File.separator + "webapps" + File.separator + "sigma" +
+                File.separator + "tests");
+        config.setPreference("TPTPDisplay", "no");
+        config.setPreference("tptpHomeDir", userHome);
+        config.setPreference("TPTP", "yes");
+        config.setPreference("typePrefix", "yes");
+        config.setPreference("userBrowserLimit", "25");
+        config.setPreference("vampire", vampireExec);
         config.setPreference("vampireExec", vampireExec);
-        config.setPreference("tptpExec", System.getProperty("user.home") + File.separator + "workspace" + File.separator + "TPTP4X" + File.separator + "tptp4X");
-        config.setPreference("systemsDir", System.getProperty("user.home"));
+
         List<String> constituents = Arrays.asList(
-            "english_format.kif",
-            "domainEnglishFormat.kif",
-            "Merge.kif",
-            "Mid-level-ontology.kif",
-            "ArabicCulture.kif",
-            "Cars.kif",
-            "Catalog.kif",
-            "Communications.kif",
-            "CountriesAndRegions.kif",
-            "Dining.kif",
-            "Economy.kif",
-            "engineering.kif",
-            "FinancialOntology.kif",
-            "Food.kif",
-            "Geography.kif",
-            "Government.kif",
-            "Hotel.kif",
-            "Justice.kif",
-            "Languages.kif",
-            "Media.kif",
-            "MilitaryDevices.kif",
-            "Military.kif",
-            "MilitaryPersons.kif",
-            "MilitaryProcesses.kif",
-            "Music.kif",
-            "naics.kif",
-            "People.kif",
-            "QoSontology.kif",
-            "Sports.kif",
-            "TransnationalIssues.kif",
-            "Transportation.kif",
-            "TransportDetail.kif",
-            "VirusProteinAndCellPart.kif",
-            "WMD.kif"
+                "english_format.kif",
+                "domainEnglishFormat.kif",
+                "ArabicCulture.kif",
+                "Anatomy.kif",
+                "arteries.kif",
+                "Biography.kif",
+                "Cars.kif",
+                "Catalog.kif",
+                "Communications.kif",
+                "CountriesAndRegions.kif",
+                "Dining.kif",
+                "Economy.kif",
+                "emotion.kif",
+                "engineering.kif",
+                "Facebook.kif",
+                "FinancialOntology.kif",
+                "Food.kif",
+                "Geography.kif",
+                "Government.kif",
+                "Hotel.kif",
+                "Justice.kif",
+                "Languages.kif",
+                "Law.kif",
+                "Media.kif",
+                "Medicine.kif",
+                "Merge.kif",
+                "Mid-level-ontology.kif",
+                "MilitaryDevices.kif",
+                "Military.kif",
+                "MilitaryPersons.kif",
+                "MilitaryProcesses.kif",
+                "Music.kif",
+                "naics.kif",
+                "People.kif",
+                "pictureList.kif",
+                "pictureList-ImageNet.kif",
+                "QoSontology.kif",
+                "Sports.kif",
+                "TransnationalIssues.kif",
+                "Transportation.kif",
+                "TransportDetail.kif",
+                "UXExperimentalTerms.kif",
+                "VirusProteinAndCellPart.kif",
+                "Weather.kif",
+                "WMD.kif"
         );
+
         config.setKbConstituentList("SUMO", constituents);
         return config;
     }
